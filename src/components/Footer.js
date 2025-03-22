20e0b89763d14a3cf950e45c4e3af6e35b7a25dc
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-03-22 20:45:29 +0530

    footer changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,22 +8,60 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <Row className="align-item-center">
-          <Col sm={6}>
-            <img src={logo} alt="Logo" />
+        <Row className="py-4">
+          {/* Logo Section */}
+          <Col md={4} sm={12}>
+            <img src={logo} alt="Logo" className="footer-logo" />
           </Col>
-          <Col sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
+
+          {/* Quick Links Section */}
+          <Col md={4} sm={12} className="text-center text-sm-start">
+            <h5 className="footer-heading">Quick Links</h5>
+            <ul className="footer-links">
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About Us</a>
+              </li>
+              <li>
+                <a href="/skills">Skills</a>
+              </li>
+              <li>
+                <a href="/projects">Projects</a>
+              </li>
+              <li>
+                <a href="/reviews">Reviews</a>
+              </li>
+            </ul>
+          </Col>
+
+          {/* Contact Section */}
+          <Col md={4} sm={12} className="text-center text-sm-end">
+            <h5 className="footer-heading">Contact Us</h5>
+            <p>123 Main St, City, Country</p>
+            <p>Email: info@example.com</p>
+            <p>Phone: +1 234 567 890</p>
+            <div className="social-icon mt-3">
               <a href="">
-                <img src={navIcon1} />
+                <img src={navIcon1} alt="Icon 1" />
               </a>
               <a href="">
-                <img src={navIcon2} />
+                <img src={navIcon2} alt="Icon 2" />
               </a>
               <a href="">
-                <img src={navIcon3} />
+                <img src={navIcon3} alt="Icon 3" />
               </a>
             </div>
+          </Col>
+        </Row>
+
+        {/* Copyright Section */}
+        <Row className="text-center pt-4">
+          <Col sm={12}>
+            <p className="text-muted">
+              &copy; 2025 Your Company. All Rights Reserved.
+            </p>
           </Col>
         </Row>
       </Container>
