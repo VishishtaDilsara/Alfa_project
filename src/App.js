0719be16a0cf09860e4fcb58d0c1e6e3aec6cc0e
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-03-22 20:29:17 +0530

    footer.js file created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -16,6 +17,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
